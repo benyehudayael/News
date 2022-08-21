e566cea96bf0317d4dc19c03a0f5a659b3611a3c
Yael <benyehuda.yael2@gmail.com>
2022-08-21 16:35:44 +0300

    client show backend data successfully

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
+import { Item } from 'src/model/item';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-news',
   templateUrl: './news.component.html',
   styleUrls: ['./news.component.css']
 })
-export class NewsComponent implements OnInit {
+export class NewsComponent implements OnInit, OnChanges {
 
-  constructor() { }
+  items: Item[] = [];
 
-  ngOnInit(): void {
+  constructor(private dataService : DataService) {
   }
 
+  ngOnInit(): void {
+    this.dataService.getItems()
+      .subscribe(items => {
+          this.items = items;
+        });
+  }
+
+  ngOnChanges(): void {
+
+  }
 }
