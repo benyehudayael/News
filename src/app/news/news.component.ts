66b4aad7848838d26a4082b317cee1f0cbacbbb8
Yael <benyehuda.yael2@gmail.com>
2022-09-13 14:43:55 +0300

    load items

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit, OnChanges } from '@angular/core';
 import { ActivatedRoute, ParamMap, Router } from '@angular/router';
-import { Observable } from 'rxjs';
+import { fromEvent, Observable } from 'rxjs';
 import { Item } from 'src/model/item';
 import { DataService } from '../services/data.service';
+import { map} from 'rxjs/operators'
 
 @Component({
   selector: 'app-news',
@@ -16,6 +17,10 @@ export class NewsComponent implements OnInit, OnChanges {
   pageSize: number = 9
   sid: string | null
   sub: Observable<ParamMap>;
+  /*Ceck this!*/
+  content: any;
+  scroll$: Observable<any>;
+  
 
   constructor(private _Activatedroute:ActivatedRoute,
     private _router:Router,
@@ -26,12 +31,27 @@ export class NewsComponent implements OnInit, OnChanges {
 
   }
 
-  ngAfterViewInit(): void {
+  ngAfterViewInit(): void { 
     this._Activatedroute.paramMap.subscribe(params => { 
       console.log(params);
        this.sid = params.get('sid'); 
        this.loadItems();
    });
+   this.content = document.querySelector('.items-container');
+   /*what the meaning of 'content!'*/
+   this.scroll$ = fromEvent(this.content!, 'scroll').pipe(map(() => { return this.content!.scrollTop; }))
+   this.scroll$.subscribe((scrollPos) => {
+    let limit = this.content!.scrollHeight - this.content!.clientHeight;
+    if (limit - scrollPos < 2){
+      this.pageIndex++;
+      this.dataService.getItems(this.pageIndex, this.pageSize, this.sid)
+      .subscribe(items => {
+        this.items.concat(items);
+      });
+     /* this.loadingState = */
+    }
+   })
+
    // register to scroll
    // call loadItems if scroll reach the bottom
   }
